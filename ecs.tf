4c7205f21dc576fb4889ce531959a2fdd88cd016
Javad Khan <jkhan4@woolworths.com.au>
2024-12-04 12:38:43 +1100

    Use enhanced monitoring for container insights

--- ecs.tf
+++ ecs.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_cluster" "ecs" {
 
   setting {
     name  = "containerInsights"
-    value = var.container_insights ? "enabled" : "disabled"
+    value = var.container_insights ? "enhanced" : "disabled"
   }
 
   lifecycle {
