cee61e758c6a4d315d24db448d7f09b0bcb0e575
Javad Khan <jkhan4@woolworths.com.au>
2026-03-12 09:15:54 +1100

    Set default cluster capacity provider

--- ecs.tf
+++ ecs.tf
@@ -16,6 +16,15 @@ resource "aws_ecs_cluster" "ecs" {
 resource "aws_ecs_cluster_capacity_providers" "ecs" {
   cluster_name = aws_ecs_cluster.ecs.name
 
+  dynamic "default_capacity_provider_strategy" {
+    for_each = !var.fargate_only ? [1] : []
+      content {
+        capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider[0].name
+        base              = 1
+        weight            = 100
+      }  
+  }
+
   capacity_providers = compact([
     try(aws_ecs_capacity_provider.ecs_capacity_provider[0].name, ""),
     "FARGATE",
